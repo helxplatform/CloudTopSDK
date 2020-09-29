a0a4ed30e23ca3646bdf44979c0221cf5e8f8ac6
Howard Lander <howard@apples-mbp.lan>
2020-09-29 16:42:58 -0400

    required changes to build and install the current napari and python envs

--- example/napari/napari.sh
+++ example/napari/napari.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 ### Add Napari
 set -x \
-   && apt install -y git \
-   && cd /usr/local/renci \
-   && git clone https://github.com/napari/napari.git \
-   && cd napari \
-   && pwd \
-   && pip3 install -e .
+   && pip3 install napari[pyqt5]
