f4675c2f58fc063a52418d0a193afe8d2101d2cc
Howard <howard@renci.org>
2021-10-13 11:43:54 -0400

    first set of changes required to deal with the fact that the Cloudtop base is now debian 11

--- apps/gnu-octave/gnu-octave.sh
+++ apps/gnu-octave/gnu-octave.sh
@@ -3,5 +3,5 @@
 set -x \
    && apt update \
    && sudo apt install -y octave \
-   && sudo apt install -y octave-doc  octave-info octave-htmldoc \
+   && sudo apt install -y octave-doc \
    && sudo apt install -y liboctave-dev
